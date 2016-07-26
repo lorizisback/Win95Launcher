1a6f2cfc5decf179c7e37ce03aa4018a7e7f50e3
Marco Loriga <marco.loriga@xorovo.com>
2016-07-26 15:55:36 +0200

    Reworked menu

--- app/src/main/java/com/winlauncher/loriz/win95launcher/MainActivity.java
+++ app/src/main/java/com/winlauncher/loriz/win95launcher/MainActivity.java
@@ -1,16 +1,37 @@
 package com.winlauncher.loriz.win95launcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.febaisi.CustomTextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
 
     private boolean isOpen = false;
     private ImageView startNormal;
     private RelativeLayout startMenu;
+    BroadcastReceiver broadcastReceiver;
+    private final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+    private CustomTextView clockTextview;
+    private LinearLayout taskbarContainer;
+    private int menuHeight;
+    private int menuWidth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
         startNormal = (ImageView) findViewById(R.id.start_button);
         startMenu = (RelativeLayout) findViewById(R.id.start_menu_container);
+        taskbarContainer = (LinearLayout) findViewById(R.id.taskbar_container);
+        clockTextview = (CustomTextView) findViewById(R.id.clock_textview);
+        clockTextview.getPaint().setAntiAlias(false);
 
         View.OnClickListener clickList = new View.OnClickListener() {
             @Override
@@ -28,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
         };
 
         startNormal.setOnClickListener(clickList);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        menuHeight = (size.y*2)/3;
+        menuWidth = (size.x*2)/3;
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(menuWidth,menuHeight);
+        params.addRule(RelativeLayout.ABOVE,R.id.bottom_bar_container);
+        params.setMargins(7,0,0,-8);
+
+        startMenu.setLayoutParams(params);
 
 
 
@@ -52,4 +87,28 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         if (isOpen == true) isOpen = startToggler();
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        clockTextview.setText(sdf.format(new Date()));
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    clockTextview.setText(sdf.format(new Date()));
+            }
+        };
+
+        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (broadcastReceiver != null)
+            unregisterReceiver(broadcastReceiver);
+    }
+
 }
